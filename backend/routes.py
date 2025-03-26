4601e736f4f51d100612696b69cd851396c796e4
Steven Arbo <steven.arbo@icloud.com>
2025-03-26 15:31:16 -0400

    add required fields for creat_game, add delete_game and update_game 3/26

--- backend/routes.py
+++ backend/routes.py
@@ -15,6 +15,11 @@ def create_game():
     try:
         data = request.json
         
+        required_fields = ["opponent", "minutes", "points", "rebounds", "assists", "notes"]
+        for field in required_fields:
+            if field not in data:
+                return jsonify({"error":f'Missing required field: {field}'}), 400
+        
         opponent = data.get("opponent")
         minutes = data.get("minutes")
         points = data.get("points")
@@ -38,3 +43,42 @@ def create_game():
     except Exception as e:
         db.session.rollback()
         return jsonify({"error": str(e)}), 500
+    
+# delete a game
+@app.route("/api/games/<int:id>", methods=['DELETE'])
+def delete_game(id):
+    try:
+        game = Game.query.get(id)
+        if game is None:
+            return jsonify({"error": "game not found"}), 404
+        
+        db.session.delete(game)
+        db.session.commit()
+        
+        return jsonify({"msg":"game deleted successfully"})
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error":str(e)}), 500
+
+# update a game
+@app.route('/api/games/<int:id>', methods=['PATCH'])
+def update_game(id):
+    try:
+        game = Game.query.get(id)
+        if game is None:
+            return jsonify({"error": "game not found"}), 404
+        
+        data = request.json
+        
+        game.opponent = data.get("opponent", game.opponent)
+        game.minutes = data.get("minutes", game.minutes)
+        game.points = data.get("points", game.points)
+        game.rebounds = data.get("rebounds", game.rebounds)
+        game.assists = data.get("assists", game.assists)
+        game.notes = data.get("notes", game.notes)
+        
+        db.session.commit()
+        return jsonify({"msg":"game updated successfully"})
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error":str(e)}), 500
